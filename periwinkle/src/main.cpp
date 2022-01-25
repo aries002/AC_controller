3e084283f3d0f54763161d93088cb193ca38056d
ARIES002 <alfianputra19@gmail.com>
2022-01-25 20:00:28 +0700

    almost_done

--- periwinkle/src/main.cpp
+++ periwinkle/src/main.cpp
@@ -1,37 +1,178 @@
+#define VERSION 0.01
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 #include <LittleFS.h>
 #include <PubSubClient.h>
+// #include "DHT.h"
+#include <PZEM004Tv30.h>
+#include <Wire.h>
+// #include "Adafruit_SHTC3.h"
+
+
+
 #include "config.esp"
-
-//---------------------------------SYSTEM DECLARATION---------------------------------------
+//---------------------------------SYSTEM VARIABLES---------------------------------------
 unsigned long uptime = 0;
-//---------------------------------NETWORK DECLARATION--------------------------------------
+bool reboot = false;
+bool reset = false;
+bool config = false;
+char *token = TOKEN;
+char *device_name;
+char *device_id;
+bool debug = true;
+int status_pin = STATUS_PIN;
+bool updating = false;
+String update_url;
 
-char* wifi_cl_ssid, wifi_cl_pass, wifi_ap_ssid, wifi_ap_pass;
-char wifi_mode;
+//---------------------------------SENSOR VARIABLES---------------------------------------
+bool sensor_ac_status = false;
+bool sensor_ac_command = false;
+unsigned long sensor_last_update = 0;
+unsigned long sensor_interval_update = SENSOR_UPDATE_INTERVAL;
+unsigned long sensor_data_sender_interval = SENSOR_UPDATE_INTERVAL;
 
-//---------------------------------MQTT DECLARATION-----------------------------------------
-//---------------------------------WEBSOCKET DECLARATION------------------------------------
 
+//---------------------------------NETWORK VARIABLES--------------------------------------
+// char* wifi_cl_ssid = "wireless@umsida.ac.id";
+// char* wifi_cl_pass = "AhmadDahlan1868";
+// char* wifi_ap_ssid = "Almost Smart";
+// char* wifi_ap_pass = NULL;
+char* wifi_ssid = WIFI_SSID;
+char* wifi_pass = WIFI_PASS;
+// char* wifi_mode = "c";
+bool wifi_mode = WIFI_MODE;
+bool net_status = false;
+
+//---------------------------------CONTROLLER VARIABLES-----------------------------------
+char *controller_token;
+char *controller_host;
+char *sensor_update_path;
+
+unsigned long controller_last_update = 0;
+
+//---------------------------------MQTT VARIABLES-----------------------------------------
+// char *mqtt_broker_ip;
+// char *mqtt_topic_sub;
+// char *mqtt_topic_pub;
+// char *mqtt_client_id;
+// char *mqtt_client_user;
+// char *mqtt_client_pass;
+// unsigned long mqtt_last_dc = 0;
+// unsigned long mqtt_send_interval = 10000;
+// unsigned long mqtt_send_last = 0;
+// bool mqtt_login = false;
+// bool mqtt_enable = false;
+
+//---------------------------------WEBSOCKET VARIABLES------------------------------------
+
+
+//---------------------------------SETUP LIBRARY--------------------------------------------
 AsyncWebServer server(HTTP_PORT);
-AsyncWebSocket ws("/ws");
+AsyncWebSocket ws(WEBSOCKET_PATH);
 
 
 #include "helper.esp"
-#include "configurator.esp"
+#include "ota_update.esp"
+#include "sensor.esp"
+#include "system.esp"
 #include "networks.esp"
-#include "mqtt.esp"
-#include "websocket.esp"
-
+#include "webserver.esp"
 
 void ICACHE_FLASH_ATTR setup(){
+  //serial setup
   Serial.begin(115200);
-  Serial.print(System_name);
+  Serial.println();
+
+  //sensor initialize
+  sensor_setup();
+
+  // dht.begin();
+
+
+//start file system
+  if(!LittleFS.begin()){
+    Log("Unable to mount File System",0);
+    Log("Formating file system",1);
+    LittleFS.format();
+    Serial.println("REBOOTING IN");
+    for (uint8_t t = 5; t > 0; t--) {
+      Serial.printf("%d...\n", t);
+      Serial.flush();
+      delay(1000);
+    }
+    ESP.restart();
+  }
+
+  //start load configuration
+  
+  // generate device id and device name
+  device_id = strdup(gen_device_id().c_str());
+  device_name = strdup(gen_device_name().c_str());
+  //load configuration
+  bool sts_sys_conf = true;// for debuging remove after finish
+  bool sts_net_conf = true;//same
+  // bool sts_sys_conf = config_load_system();
+  // bool sts_net_conf = config_load_network();
+  //print device info
+  Log("Device id = " + String(device_id),2);
+  Log("Device name = " + String(device_name),2);
+  //check if configuration is success
+  if(sts_sys_conf){
+    config = true;
+    //check if network configuration is success then start network
+    if(sts_net_conf){
+      net_status = wifi_begin();
+    }
+    else{
+      // if configuration failed or did't exist start AP
+      wifi_ap_setup(device_name, WIFI_FAIL_PASS);
+    }
+  }
+  // start web server
+  webserver();
+  if(config && net_status){
+
+  }
+  test_config();
 }
 
 void IRAM_ATTR loop(){
+  //get system clock and uptime
   uptime = millis();
+  //reboot the system if required
+  if(reset){
+    reset_config();
+  }
+  if(reboot){
+    Log("Device rebooting...",2);
+    ESP.restart();
+  }
+
+  if(config){
+    //update sensor data
+    update_sensor_ac();
+    ac_controller();
+    if(uptime - sensor_last_update >= sensor_interval_update){
+      Log("updating sensor");
+      update_sensor();
+      //send sensor data to server
+      if(net_status){
+        sensor_data_sender();
+      }
+      sensor_last_update = uptime;
+    }
+  }
+  if(updating){
+    do_update();
+  }
+  if(!updating){
+    //keep the system alive
+    breathe();
+  }
 }
---
+++ sensor_test/sensor_test.ino
@@ -0,0 +1,119 @@
+#define PIN_POWER 0
+#define PIN_SIGNAL 13
+#include "SparkFun_SHTC3.h" // Click here to get the library: http://librarymanager/All#SparkFun_SHTC3
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+#define PZEM_RX_PIN 12
+#define PZEM_TX_PIN 14
+
+
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+
+SHTC3 mySHTC3;              // Declare an instance of the SHTC3 class
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_POWER, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_SIGNAL, INPUT);
+  
+  Serial.begin(115200);
+  while(Serial == false){};// Begin Serial 
+  Serial.println("SHTC3 Example 1 - Basic Readings");
+  Wire.begin();
+  Serial.print("Beginning sensor. Result = ");           // Most SHTC3 functions return a variable of the type "SHTC3_Status_TypeDef" to indicate the status of their execution 
+  errorDecoder(mySHTC3.begin());                              // To start the sensor you must call "begin()", the default settings use Wire (default Arduino I2C port)
+  Serial.println();
+  Serial.println("\n\n");
+  Serial.println("Waiting for 5 seconds so you can read this info ^^^");
+
+  delay(5000);  
+}
+
+void loop() {
+  Serial.print("PIN Status : ");
+  Serial.println(digitalRead(PIN_SIGNAL));
+  SHTC3_Status_TypeDef result = mySHTC3.update();             // Call "update()" to command a measurement, wait for measurement to complete, and update the RH and T members of the object
+  printInfo();                                                // This function is used to print a nice little line of info to the serial port
+
+      Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
+        Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+    }
+
+    Serial.println();
+
+    
+  delay(190);
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_POWER, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+  digitalWrite(PIN_POWER, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(4000);
+}
+
+///////////////////////
+// Utility Functions //
+///////////////////////
+void printInfo()
+{
+  if(mySHTC3.lastStatus == SHTC3_Status_Nominal)              // You can also assess the status of the last command by checking the ".lastStatus" member of the object
+  {
+    Serial.print("RH = "); 
+    Serial.print(mySHTC3.toPercent());                        // "toPercent" returns the percent humidity as a floating point number
+    Serial.print("%, T = "); 
+    Serial.print(mySHTC3.toDegC());                           // "toDegF" and "toDegC" return the temperature as a flaoting point number in deg F and deg C respectively 
+    Serial.println(" deg C"); 
+  }
+  else
+  {
+    Serial.print("Update failed, error: "); 
+    errorDecoder(mySHTC3.lastStatus);
+    Serial.println();
+  }
+}
+
+void errorDecoder(SHTC3_Status_TypeDef message)                             // The errorDecoder function prints "SHTC3_Status_TypeDef" resultsin a human-friendly way
+{
+  switch(message)
+  {
+    case SHTC3_Status_Nominal : Serial.print("Nominal"); break;
+    case SHTC3_Status_Error : Serial.print("Error"); break;
+    case SHTC3_Status_CRC_Fail : Serial.print("CRC Fail"); break;
+    default : Serial.print("Unknown return code"); break;
+  }
+}
